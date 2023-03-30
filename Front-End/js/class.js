ee1191ae03aa51768b917ee65a6223f8e356d8bf
CAMILA ALVES PINHEIRO <22282078@CFP127ALUNO.LOCAL>
2023-03-30 11:13:04 -0300

    Começando a consumir API Postman

--- Front-End/js/class.js
+++ Front-End/js/class.js
@@ -1,11 +1,10 @@
 'use strict'
 
-import { alunos } from "../js/alunos.js"
-
-
 const criarCard = (student) => {
     const card = document.createElement('li')
-    if (student.status == "Cursando"){
+    card.classList.add('card')
+    card.id = 'card'
+    if (student.status == "Cursando") {
         card.classList.add('card-studying')
     } else {
         card.classList.add('card-finalized')
@@ -18,17 +17,39 @@ const criarCard = (student) => {
     const name = document.createElement('h3')
     name.textContent = student.nome
 
-
     card.append(img, name)
 
     return card
 }
 
-const carregarAlunos = () => {
+const carregarAlunos = async () => {
+    const url = `http://localhost:8080/v1/lion-school/alunos`
+
+    const response = await fetch(url)
+    const data = await response.json()
+    let teste = data.alunos
+
     const container = document.getElementById('list-students')
-    const cards = alunos.map(criarCard)
+    const cards = teste.map(criarCard)
 
     container.replaceChildren(...cards)
 }
 
-carregarAlunos() 
+carregarAlunos()
+
+const clearListStudents = () => {
+    const cards = document.getElementById('card')
+    cards.forEach((card) => card.remove())
+}
+
+const statusSelect = document.getElementById(`select`)
+statusSelect.addEventListener(`change`, (el) => {
+    const status = statusSelect.value
+    const studentsFilter = await getStudentsByFilter(courseInitial, status.toLowerCase())
+
+    if (status == `status`) {
+        location.reload()
+    }
+    clearListStudents()
+    criarCard(studentsFilter)
+})
